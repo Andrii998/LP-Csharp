3b96a14dccb6e99e825f747bec4b7e7531d6371e
Andrii998 <andriikurchak9@gmail.com>
2026-05-22 16:13:46 +0100

    Exercício4

---
+++ Exercício4/Exercício4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Exercício4
+{
+    public static void Main(string[] args)
+    {
+        int idade = 12;
+
+        if (idade < 11)
+        {
+            Console.WriteLine("E uma criança: " + idade);
+        }
+        else if (idade >= 11 && idade < 18)
+        {
+            Console.WriteLine("E Adolescente: " + idade);
+        }
+        else
+        {
+            Console.WriteLine("E um adulto: " + idade);
+        }
+
+    }   
+}
