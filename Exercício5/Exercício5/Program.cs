1e102424043b8498eb6389fb87d233b3c1ec32df
Andrii998 <andriikurchak9@gmail.com>
2026-05-22 16:14:12 +0100

    Exercício5

---
+++ Exercício5/Exercício5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Exercício5
+{
+    public static void Main(string[] args)
+    {
+        int idade = 29;
+        bool casado = true;
+
+        if (idade < 18)
+        {
+        Console.WriteLine("Es uma criança com idade: " + idade);
+        }
+        else if (idade >= 18 && idade < 65)
+        {
+            if (casado = true)
+            {
+                Console.WriteLine("Adulto casado");
+            }
+            else
+            {
+                Console.WriteLine("Adulto solteiro");
+            }
+        }
+        else
+        { 
+        Console.WriteLine("És sénior com idade : " + idade);
+        }
+
+    }   
+}
