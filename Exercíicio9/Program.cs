59d227d01db5c62b39f9479c19c7466cd044eb02
Andrii998 <andriikurchak9@gmail.com>
2026-06-15 13:02:35 +0100

    Exercicio9

---
+++ Exercíicio9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Serialization;
+public class Exercício10
+{
+    public static void Main(string[] args)
+    {
+        int soma;
+        for (int counter = 1; counter <= 50; counter++)
+        {
+            if (counter % 2 != 0)
+            {
+                Console.WriteLine(counter);
+            }
+
+            }
+         
+        }
+    }
+}
